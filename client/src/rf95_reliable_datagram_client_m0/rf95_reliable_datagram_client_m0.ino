71f75700e9d48df8e8d42cccc909ade0b2aedd2e
TW <tw58800@gmail.com>
2023-07-24 17:13:47 +0100

    send gps data

--- client/src/rf95_reliable_datagram_client_m0/rf95_reliable_datagram_client_m0.ino
+++ client/src/rf95_reliable_datagram_client_m0/rf95_reliable_datagram_client_m0.ino
@@ -1,16 +1,8 @@
-
-
-// rf95_reliable_datagram_client.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging client
-// with the RHReliableDatagram class, using the RH_RF95 driver to control a RF95 radio.
-// It is designed to work with the other example rf95_reliable_datagram_server
-// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W
- 
 #include <RadioHead.h>
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
 #include <SPI.h>
+#include <Adafruit_GPS.h>
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
@@ -19,10 +11,12 @@
 #define RFM95_CS    8
 #define RFM95_INT   3
 #define RFM95_RST   4
+#define GPSSerial Serial1 // what's the name of the hardware serial port?
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
 
 // Singleton instance of the radio driver
-//RH_RF95 driver;
-//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
 RH_RF95 driver(RFM95_CS, RFM95_INT);
 
 float frequency = 868.0;
@@ -30,53 +24,114 @@ float frequency = 868.0;
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 
-// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
-//#define Serial SerialUSB
+// Connect to the GPS on the hardware port
+Adafruit_GPS GPS(&GPSSerial);
+
+uint32_t timer = millis();
+
 
 void setup() 
 {
-  // Rocket Scream Mini Ultra Pro with the RFM95W only:
-  // Ensure serial flash is not interfering with radio communication on SPI bus
-//  pinMode(4, OUTPUT);
-//  digitalWrite(4, HIGH);
-
-  Serial.begin(9600);
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic parsing test!");
   while (!Serial) ; // Wait for serial port to be available
   if (!manager.init())
     Serial.println("init failed");
+  
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-	// Setup ISM frequency
 	driver.setFrequency(frequency);
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 2 to 20 dBm:
   driver.setTxPower(20, false);
-
-  // If you are using Modtronix inAir4 or inAir9, or any other module which uses the
-  // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for 0 to 15 dBm and with useRFO true. 
-  // Failure to do that will result in extremely low transmit powers.
-  //  driver.setTxPower(14, true);
-
   // You can optionally require this module to wait until Channel Activity
   // Detection shows no activity on the channel before transmitting by setting
   // the CAD timeout to non-zero:
   //  driver.setCADTimeout(10000);
+
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+
+  // Ask for firmware version
+  GPSSerial.println(PMTK_Q_RELEASE);
 }
 
-// uint8_t data[] = "Hello World!";
 // Dont put this on the stack:
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 int counter = 0;
 
 void loop()
 {
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  //if (GPSECHO)
+  //  if (c) Serial.print(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 5000) {
+    timer = millis(); // reset the timer
+    /*
+    Serial.print("\nTime: ");
+    if (GPS.hour < 10) { Serial.print('0'); }
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    if (GPS.minute < 10) { Serial.print('0'); }
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    if (GPS.seconds < 10) { Serial.print('0'); }
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    if (GPS.milliseconds < 10) {
+      Serial.print("00");
+    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+      Serial.print("0");
+    }
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    */
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      Serial.print("Antenna status: "); Serial.println((int)GPS.antenna);
+    }
+  }
+
   Serial.println("Sending to rf95_reliable_datagram_server");
     
   // Send a message to manager_server
-  snprintf((char *)buf, sizeof(buf), "to server counter=%d", ++counter);
+  snprintf((char *)buf, sizeof(buf), GPS.lastNMEA()); //"to server counter=%d", ++counter);
 
   if (manager.sendtoWait(buf, sizeof(buf), SERVER_ADDRESS))
   {
--- server/src/Lora-p2p.cpp
+++ server/src/Lora-p2p.cpp
@@ -3,7 +3,7 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "c:/Users/timwh/Projects/tracker/server/src/Lora-p2p.ino"
+#line 1 "c:/Users/TW/Documents/GitHub/tracker/server/src/Lora-p2p.ino"
 // This #include statement was automatically added by the Particle IDE.
 //#include <RF9X-RK.h>
 
@@ -17,15 +17,22 @@
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
 #include <SPI.h>
+#include <Adafruit_GPS.h>
 
 void setup();
 void loop();
-#line 15 "c:/Users/timwh/Projects/tracker/server/src/Lora-p2p.ino"
+#line 16 "c:/Users/TW/Documents/GitHub/tracker/server/src/Lora-p2p.ino"
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(AUTOMATIC);
+
+// Connect to the GPS on the hardware port
+//Adafruit_GPS GPS(&Serial1);
+Adafruit_GPS GPS;
+
+uint32_t timer = millis();
 
 // Singleton instance of the radio driver
 RH_RF95 driver(D6, D2);
@@ -98,11 +105,51 @@ void loop()
 			}
 
 			snprintf((char *)buf, sizeof(buf), "request=%d rssi=%d", request, driver.lastRssi());
+			
+    		if (!GPS.parse((char*)buf)) // this also sets the newNMEAreceived() flag to false
+      			return; // we can fail to parse a sentence in which case we should just wait for another
 
 			// Send a reply back to the originator client
 			if (!manager.sendtoWait(buf, strlen((char *)buf), from))
 				Serial.println("sendtoWait failed");
 		}
 	}
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 5000) {
+    timer = millis(); // reset the timer
+    /*
+    Serial.print("\nTime: ");
+    if (GPS.hour < 10) { Serial.print('0'); }
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    if (GPS.minute < 10) { Serial.print('0'); }
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    if (GPS.seconds < 10) { Serial.print('0'); }
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    if (GPS.milliseconds < 10) {
+      Serial.print("00");
+    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+      Serial.print("0");
+    }
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    */
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      //Serial.print("Antenna status: "); Serial.println((int)GPS.antenna);  // GPS class has no member 'antenna'
+    }
+  }
 }
 
